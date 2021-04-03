96d001a345d322a30101335f1524d3c4284a030a
JoaoVSouto <joaovitorss99@hotmail.com>
2021-04-03 12:09:13 -0300

    Remove Main styles and tests

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,23 +1,3 @@
-import * as S from './styles';
-
 export default function Main() {
-  return (
-    <S.Wrapper>
-      <S.Logo
-        src="https://raw.githubusercontent.com/React-Avancado/boilerplate/7df00794c0d0e6cf75c78dbd9530803e8fbfa72f/public/img/logo.svg"
-        alt="Imagem de um átomo e React Avançado escrito ao lado."
-      />
-
-      <S.Title>React Avançado</S.Title>
-
-      <S.Description>
-        TypeScript, ReactJS, NextJS e Styled Components
-      </S.Description>
-
-      <S.Illustration
-        src="https://raw.githubusercontent.com/React-Avancado/boilerplate/7df00794c0d0e6cf75c78dbd9530803e8fbfa72f/public/img/hero-illustration.svg"
-        alt="Um desenvolvedor de frente para uma tela com código."
-      />
-    </S.Wrapper>
-  );
+  return <h1>My Trips</h1>;
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092b" />
+        <meta
+          name="description"
+          content="A simple app for marking places that I've already been."
+        />
       </Head>
 
       <Component {...pageProps} />
