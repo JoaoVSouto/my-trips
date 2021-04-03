9b889e1245fa8ccaa8445e05bd247047c36cc8cf
JoaoVSouto <joaovitorss99@hotmail.com>
2021-04-03 11:51:28 -0300

    [B] Configure PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,11 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>My Trips</title>
+
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092b" />
       </Head>
 
       <Component {...pageProps} />
