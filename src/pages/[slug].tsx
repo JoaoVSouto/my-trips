43cfc91c18f5a59f6bb70f4de66de32254ec5c0f
JoaoVSouto <joaovitorss99@hotmail.com>
2021-04-10 16:30:50 -0300

    Configure GraphQL codegen for automatic typings

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 
 import client from 'graphql/client';
 import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries';
+import { GetPageBySlugQuery, GetPagesQuery } from 'graphql/generated/graphql';
 
 import PageTemplate, { PageTemplateProps } from 'templates/Pages';
 
@@ -17,7 +18,9 @@ export default function Page({ heading, body }: PageTemplateProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const { pages } = await client.request(GET_PAGES, { first: 3 });
+  const { pages } = await client.request<GetPagesQuery>(GET_PAGES, {
+    first: 3,
+  });
 
   const paths = pages.map(({ slug }) => ({
     params: { slug },
@@ -30,7 +33,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { page } = await client.request(GET_PAGE_BY_SLUG, {
+  const { page } = await client.request<GetPageBySlugQuery>(GET_PAGE_BY_SLUG, {
     slug: String(params?.slug),
   });
 
