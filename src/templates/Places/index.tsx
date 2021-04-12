efb3ccfeaa4c1bde5240efa91506913c7f5cfd24
JoaoVSouto <joaovitorss99@hotmail.com>
2021-04-12 11:03:46 -0300

    Generate static place pages

---
+++ src/templates/Places/index.tsx
@@ -0,0 +1,34 @@
+interface ImageProps {
+  url: string;
+  height: number;
+  width: number;
+}
+
+export interface PlacesTemplateProps {
+  place: {
+    slug: string;
+    name: string;
+    description: {
+      html: string;
+    };
+    gallery: ImageProps[];
+  };
+}
+
+export default function PlacesTemplate({ place }: PlacesTemplateProps) {
+  return (
+    <>
+      <h1>{place.name}</h1>
+
+      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
+
+      {place.gallery.map((image, index) => (
+        <img
+          key={`photo-${index}-${image.url}`}
+          src={image.url}
+          alt={place.name}
+        />
+      ))}
+    </>
+  );
+}
