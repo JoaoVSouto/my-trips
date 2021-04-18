c29e1b31865099055bc44de0d5c3841f5c3e7348
JoaoVSouto <joaovitorss99@hotmail.com>
2021-04-18 13:04:52 -0300

    Add background to LinkWrapper

--- src/components/LinkWrapper/styles.ts
+++ src/components/LinkWrapper/styles.ts
@@ -6,6 +6,8 @@ export const Wrapper = styled.div`
   top: var(--medium);
   right: var(--medium);
   cursor: pointer;
+  background-color: var(--background);
+  border-radius: 50%;
 
   a {
     color: var(--white);
